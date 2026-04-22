c8f050a6803ec426e80ab5ab75a9c91d3f1a5589
dorofeeva130 <ulya.dorofeeva04@mail.ru>
2026-04-23 00:05:51 +0500

    обновлен код

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -1,13 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 def test_categories_switch():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Chrome()
     wait = WebDriverWait(driver, 10)
 
     try:
